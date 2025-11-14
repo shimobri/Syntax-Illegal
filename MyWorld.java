5c7b21902280c948f12ced9c15ae5525113b880a
ijhc <120232742+ijhc@users.noreply.github.com>
2025-11-13 21:15:40 -0800

    Create MyWorld.java

---
+++ MyWorld.java
@@ -0,0 +1,54 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    private double gameTime = 0.0;
+    private int score = 0;
+    
+    private boolean rotate;
+    private boolean drop;
+    private boolean store;
+    
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 40x40 pixels
+        super(10, 20, 40);
+        prepare();
+
+        // Set a gray background (WIP)
+        /* GreenfootImage cell = new GreenfootImage(1, 1);
+        cell.setColor(Color.DARK_GRAY);
+        cell.fill();
+        setBackground(cell); */
+        
+        GreenfootImage cell = getBackground();
+        cell.setColor(Color.WHITE); // Set color for grid lines
+
+        // Draw horizontal lines
+        for (int y = 1; y < getHeight(); y++) {
+            cell.drawLine(0, y * getCellSize(), getWidth() * getCellSize(), y * getCellSize());
+        }
+
+        // Draw vertical lines
+        for (int x = 1; x < getWidth(); x++) {
+            cell.drawLine(x * getCellSize(), 0, x * getCellSize(), getHeight() * getCellSize());
+        }
+    }
+    
+    private void prepare() {
+        Tetromino mino = new Tetromino();
+        addObject(mino, 5, 0);
+        mino.setLocation(5, 0);
+        mino.turn(90);
+    }
+}
